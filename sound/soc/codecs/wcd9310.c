593dcbb3b1e4d937746e0445a3d9361d3d90908b
Bryan Smith <zarboz@The-Box.(none)>
2012-09-13 13:16:11 -0600

    adding driver support for the previously added audio KHZ support

--- sound/soc/codecs/wcd9310.c
+++ sound/soc/codecs/wcd9310.c
@@ -39,7 +39,7 @@
 //htc audio --
 
 #define WCD9310_RATES (SNDRV_PCM_RATE_8000|SNDRV_PCM_RATE_16000|\
-			SNDRV_PCM_RATE_32000|SNDRV_PCM_RATE_48000)
+			SNDRV_PCM_RATE_32000|SNDRV_PCM_RATE_48000|SNDRV_PCM_RATE_9600)
 
 #define NUM_DECIMATORS 10
 #define NUM_INTERPOLATORS 7
@@ -3024,6 +3024,10 @@ static int tabla_hw_params(struct snd_pcm_substream *substream,
 	case 48000:
 		tx_fs_rate = 0x03;
 		rx_fs_rate = 0x60;
+		break;
+	case 96000:
+		tx_fs_rate = 0x04;
+		rx_fs_rate = 0x80;
 		break;
 	default:
 		pr_err("%s: Invalid sampling rate %d\n", __func__,
